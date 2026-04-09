b2727f92bd4bbc527ad81e2d7829ae4b9ea9d454
Gerunia <braineaterofficial7200@gmail.com>
2026-04-10 00:55:28 +0300

    language test v1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.seleniumhq.selenium:selenium-java:4.18.1")
 
 }
