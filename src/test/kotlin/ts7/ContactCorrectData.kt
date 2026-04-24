af624bdd83d65f4688c85e5e739b9b63c7931107
Gerunia <braineaterofficial7200@gmail.com>
2026-04-24 04:55:21 +0300

    ts7 uncommented

--- src/test/kotlin/ts7/ContactCorrectData.kt
+++ src/test/kotlin/ts7/ContactCorrectData.kt
@@ -2,19 +2,22 @@ package ts7
 
 import BaseTest
 import Browser
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.openqa.selenium.By
+import kotlin.test.Test
 
 abstract class ContactCorrectData(browser: Browser) : BaseTest(browser) {
-//    @Test
-//    fun testValidContactData() {
-//        // Переход в аккаунт
-//        driver.findElement(By.xpath("//a[@href='/account']")).click()
-//
-//        driver.findElement(By.xpath("//input[@name='email']")).clear()
-//        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("test@mail.com")
-//
-//        driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click()
-//
-//        val success = driver.findElement(By.xpath("//div[contains(@class,'success')]"))
-//        assertTrue(success.isDisplayed)
-//    }
+    @Test
+    fun testValidContactData() {
+        // Переход в аккаунт
+        driver.findElement(By.xpath("//a[@href='/account']")).click()
+
+        driver.findElement(By.xpath("//input[@name='email']")).clear()
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("test@mail.com")
+
+        driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click()
+
+        val success = driver.findElement(By.xpath("//div[contains(@class,'success')]"))
+        assertTrue(success.isDisplayed)
+    }
 }
--- src/test/kotlin/ts7/ContactWrongData.kt
+++ src/test/kotlin/ts7/ContactWrongData.kt
@@ -2,18 +2,21 @@ package ts7
 
 import BaseTest
 import Browser
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.openqa.selenium.By
+import kotlin.test.Test
 
 abstract class ContactWrongData(browser: Browser) : BaseTest(browser) {
-//    @Test
-//    fun testInvalidDisplayName() {
-//        driver.findElement(By.xpath("//a[@href='/account']")).click()
-//
-//        driver.findElement(By.xpath("//input[@name='displayName']")).clear()
-//        driver.findElement(By.xpath("//input[@name='displayName']")).sendKeys("!!!")
-//
-//        driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click()
-//
-//        val error = driver.findElement(By.xpath("//div[contains(@class,'error')]"))
-//        assertTrue(error.isDisplayed)
-//    }
+    @Test
+    fun testInvalidDisplayName() {
+        driver.findElement(By.xpath("//a[@href='/account']")).click()
+
+        driver.findElement(By.xpath("//input[@name='displayName']")).clear()
+        driver.findElement(By.xpath("//input[@name='displayName']")).sendKeys("!!!")
+
+        driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click()
+
+        val error = driver.findElement(By.xpath("//div[contains(@class,'error')]"))
+        assertTrue(error.isDisplayed)
+    }
 }
