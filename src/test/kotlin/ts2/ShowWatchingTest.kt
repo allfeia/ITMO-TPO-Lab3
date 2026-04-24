e810b1e0b36f141e2c0540eead653b4598fb4740
Gerunia <braineaterofficial7200@gmail.com>
2026-04-24 03:58:11 +0300

    ts2 updated

--- src/test/kotlin/ts2/ShowWatchingTest.kt
+++ src/test/kotlin/ts2/ShowWatchingTest.kt
@@ -27,4 +27,56 @@ abstract class ShowWatchingTest(browser: Browser) : BaseTest(browser) {
             { assertTrue(driver.findElements(By.xpath("//div[contains(@class,'chat')]")).isNotEmpty()) }
         )
     }
+
+    @Test
+    fun openSmallShow() {
+        val wait = WebDriverWait(driver, Duration.ofSeconds(20))
+
+        // нажать на модель
+        wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//a[contains(@class,'lst_link')]")
+        )).click()
+
+        // дожидаемся загрузки плеера
+        wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//video")
+        ))
+
+        // возвращаемся на главную страницу
+        wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//img[contains(@class,'logo')]")
+        )).click()
+
+        //проверяем, что видео отображается
+        assertTrue(driver.findElements(By.xpath("//div[contains(@class, 'hover')]")).isNotEmpty())
+    }
+
+    @Test
+    fun openMedia() {
+        val wait = WebDriverWait(driver, Duration.ofSeconds(20))
+
+        // нажать на модель
+        wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//a[contains(@class,'lst_link')]")
+        )).click()
+
+        // дожидаемся загрузки плеера
+        wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//video")
+        ))
+
+        //проверяем, что секция с медиа отображается
+        assertTrue(driver.findElements(By.xpath("//div[contains(@class, 'media')]")).isNotEmpty())
+
+        //жмем на превью медиа
+        wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//img[contains(@class, 'img')]")
+        )).click()
+
+        // проверяем, что отобразился интерфейс просмотра медиа
+        assertAll(
+            { assertTrue(driver.findElements(By.xpath("//button[contains(@class, 'close')]")).isNotEmpty()) },
+            { assertTrue(driver.findElements(By.xpath("//div[contains(@class,'preview')]")).isNotEmpty()) }
+        )
+    }
 }
