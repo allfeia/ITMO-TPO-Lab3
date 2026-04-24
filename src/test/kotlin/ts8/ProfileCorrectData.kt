12a4f032d40a9c765b7396e03d23cce4e4934cd8
Gerunia <braineaterofficial7200@gmail.com>
2026-04-24 04:55:29 +0300

    ts8 uncommented

--- src/test/kotlin/ts8/ProfileCorrectData.kt
+++ src/test/kotlin/ts8/ProfileCorrectData.kt
@@ -2,18 +2,21 @@ package ts8
 
 import BaseTest
 import Browser
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.openqa.selenium.By
+import kotlin.test.Test
 
 abstract class ProfileCorrectData(browser: Browser) : BaseTest(browser) {
-//    @Test
-//    fun testValidProfileInfo() {
-//        driver.findElement(By.xpath("//a[@href='/profile']")).click()
-//
-//        driver.findElement(By.xpath("//textarea[@name='bio']")).clear()
-//        driver.findElement(By.xpath("//textarea[@name='bio']")).sendKeys("Hello, I'm a tester")
-//
-//        driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click()
-//
-//        val success = driver.findElement(By.xpath("//div[contains(@class,'success')]"))
-//        assertTrue(success.isDisplayed)
-//    }
+    @Test
+    fun testValidProfileInfo() {
+        driver.findElement(By.xpath("//a[@href='/profile']")).click()
+
+        driver.findElement(By.xpath("//textarea[@name='bio']")).clear()
+        driver.findElement(By.xpath("//textarea[@name='bio']")).sendKeys("Hello, I'm a tester")
+
+        driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click()
+
+        val success = driver.findElement(By.xpath("//div[contains(@class,'success')]"))
+        assertTrue(success.isDisplayed)
+    }
 }
--- src/test/kotlin/ts8/ProfileWrongData.kt
+++ src/test/kotlin/ts8/ProfileWrongData.kt
@@ -2,17 +2,20 @@ package ts8
 
 import BaseTest
 import Browser
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.openqa.selenium.By
+import kotlin.test.Test
 
 abstract class ProfileWrongData(browser: Browser) : BaseTest(browser) {
-//    @Test
-//    fun testInvalidProfileInfo() {
-//        driver.findElement(By.xpath("//a[@href='/profile']")).click()
-//
-//        driver.findElement(By.xpath("//textarea[@name='bio']")).sendKeys("")
-//
-//        driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click()
-//
-//        val error = driver.findElement(By.xpath("//div[contains(@class,'error')]"))
-//        assertTrue(error.isDisplayed)
-//    }
+    @Test
+    fun testInvalidProfileInfo() {
+        driver.findElement(By.xpath("//a[@href='/profile']")).click()
+
+        driver.findElement(By.xpath("//textarea[@name='bio']")).sendKeys("")
+
+        driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click()
+
+        val error = driver.findElement(By.xpath("//div[contains(@class,'error')]"))
+        assertTrue(error.isDisplayed)
+    }
 }
